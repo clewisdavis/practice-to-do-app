bf5755cbc6a3b60c9742854ac548518131572ad8
Chris <chrislewis.davis@gmail.com>
2025-01-27 08:58:42 -0500

    initialize React To-Do app with basic structure and components

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+
+// To to list functionality:
+// Add a new task
+// Mark a task as completed
+// Delete a task
+// Edit a task
+// Clear all tasks
+// view all tasks
+
+function App() {
+  
+  // add tasks and state
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      name: 'Task 1',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      name: 'Task 2',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      name: 'Task 3',
+      isCompleted: false
+    },
+  ]);
+
+  const addTask = (taskName) => {
+    const newTask = {
+      id: tasks.length + 1,
+      name: taskName,
+      isCompleted: false
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="app">
+      <h1>TO DO LIST! ✅</h1>
+      <List tasks={tasks} setTasks={setTasks} />
+      <button>Add Task</button>
+      <AddTask addTask={addTask} />
+    </div>
+  );
+}
+
+export default App;
+
+// List component
+function List({ tasks, setTasks }) {
+
+  const handleCheckboxChange = (id) => {
+    setTasks(tasks.map((task) => (
+      task.id === id ? { ...task, isCompleted: !task.isCompleted} : task  // toggle the isCompleted value
+    )))
+  }
+
+  const handleDeleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  return (
+    <ul className="todo-list">
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <input
+            type="checkbox"
+            checked={task.isCompleted}
+            onChange={() => handleCheckboxChange(task.id)}
+            id={`task${task.id}`}
+          />
+          <label htmlFor={`task${task.id}`}>{task.name}</label>
+          <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// AddTask Component
+function AddTask({ addTask }) {
+  const [task, setTask] = useState('');
+
+  const handleAddTask = () => {
+    if (task) {
+      addTask(task);
+      setTask('');
+    }
+  }
+
+  return (
+    <div className="add-task">
+      <label htmlFor="">Add Task</label>
+      <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={handleAddTask}>Add</button>
+    </div>
+  )
+}
